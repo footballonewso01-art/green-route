b4da37a98abe7cd375d4e310f3bfb937a00f7605
footballonewso01-art <footballonewso01@gmail.com>
2026-03-05 01:01:26 +0200

    style(profile): push start of avatar fade down by another 7 percent (41% total)

--- src/pages/PublicProfile.tsx
+++ src/pages/PublicProfile.tsx
@@ -168,7 +168,7 @@ export default function PublicProfile() {
             </div>
           )}
           {/* Fade to Black Overlay - Lower and softer */}
-          <div className="absolute -bottom-1 left-0 w-full h-[48%] bg-gradient-to-t from-black from-15% via-black/60 to-transparent transition-all duration-700 pointer-events-none" />
+          <div className="absolute -bottom-1 left-0 w-full h-[41%] bg-gradient-to-t from-black from-15% via-black/60 to-transparent transition-all duration-700 pointer-events-none" />
         </div >
 
         {/* Profile Content */}
